aca7c890878375a894f90d2c4bdbbcb73f875896
Fernanda Hiarita <fehiarita.15@gmail.com>
2024-05-01 20:19:47 -0300

    metodo cadastrarUsuario
    criação do metodo

--- apinew/api/src/main/java/com/postgresql/api/Model/Usuario.java
+++ apinew/api/src/main/java/com/postgresql/api/Model/Usuario.java
@@ -1,6 +1,9 @@
 package com.postgresql.api.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -10,8 +13,20 @@ import lombok.Data;
 @Table(name = "usuario")
 public class Usuario {
     @Id
+    // indica que o banco de dados irá gerar automaticamente os valores para o campo id conforme os registros são inseridos na tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    // mapea um campo da entidade para uma coluna na tabela 
+    @Column(nullable = false, length = 100)
     private String nome;
+
+    @Column(nullable = false, unique = true,length = 50)
+    private String username;
+
+    @Column(nullable = false,unique = true,length = 100)
     private String email;
-    private String nivelHabilidade;
+
+    @Column(nullable = false)
+    private String senha;
 }
--- apinew/api/src/main/java/com/postgresql/api/Repository/UsuarioRepository.java
+++ apinew/api/src/main/java/com/postgresql/api/Repository/UsuarioRepository.java
@@ -2,12 +2,18 @@ package com.postgresql.api.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.postgresql.api.Model.Usuario;
+
 // recurso RESTful:pedaço de dados ou funcionalidade que pode ser acessado e manipulado através de requisições HTTP.
 // indica que essa interface deve ser exposta como um recurso REST
 @RepositoryRestResource
+@Repository
 // a parte de "extends" fornece métodos prontos para realizar operações CRUD em objetos Usuario armazenados em um banco de dados.
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
